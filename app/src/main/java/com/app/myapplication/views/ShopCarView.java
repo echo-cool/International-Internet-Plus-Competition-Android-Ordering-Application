a7c997c7102aba577caef43d04c1acda3dcb9408
bug_creator <wenrui.liu@ucdconnect.ie>
2021-05-29 21:14:59 +0800

    update shopcar

---
+++ app/src/main/java/com/app/myapplication/views/ShopCarView.java
@@ -0,0 +1,33 @@
+package com.app.myapplication.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.app.myapplication.R;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.text.DecimalFormat;
+
+public class ShopCarView extends ConstraintLayout {
+
+    private Context mContext;
+
+    public ShopCarView(@NonNull @NotNull Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs) {
+        super(context, attrs);
+        inflate(context, R.layout.view_car,this);
+        mContext=context;
+    }
+
+    public void setPrice(double price){
+        if(price!=0)
+            ((TextView)findViewById(R.id.textView7)).setText(new DecimalFormat("0.00").format(price));
+        else
+            ((TextView)findViewById(R.id.textView7)).setText("0");
+    }
+}
