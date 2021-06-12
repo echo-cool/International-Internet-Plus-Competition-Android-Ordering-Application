7024c16df54ffcb753b8d93f7d324327e9fe2a05
bug_creator <wenrui.liu@ucdconnect.ie>
2021-06-12 22:59:56 +0800

    fix bug

---
+++ studentclient/src/main/java/com/app/myapplication/ScheduleActivity.java
@@ -0,0 +1,14 @@
+package com.app.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
